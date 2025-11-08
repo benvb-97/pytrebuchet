c55ba28613e334d30932b5364b27249ba5c5357a
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2025-11-08 17:21:59 +0100

    add simple script to automatically reformat repository using black/isort

---
+++ dev/formatter.py
@@ -0,0 +1,19 @@
+"""
+Small script to quickly reformat all project files using the 'black' and 'isort' package.
+
+You should install black/isort using pip install to your main python installation if you'd like to use this code.
+Black/isort is not a requirement for the main code to run.
+"""
+
+if __name__ == "__main__":
+    import os
+
+    # Get directory of the project
+    # This file resides in the 'dev' folder, so we go one level up
+    project_folder_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    # Reformat import statements
+    os.system(f"isort {project_folder_path}")
+
+    # Reformat general code
+    os.system(f"black {project_folder_path}")
