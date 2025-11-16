1dd684b60fa07da7cdd588a546aa42233eaac3da
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2025-11-16 20:05:03 +0100

    no message

--- src/pytrebuchet/optimization.py
+++ src/pytrebuchet/optimization.py
@@ -142,7 +142,6 @@ class DesignOptimizer:
                 bounds=self._bounds,
                 constraints=constraints,
                 options=options,
-                tol=1e-6
             )
 
         return result
