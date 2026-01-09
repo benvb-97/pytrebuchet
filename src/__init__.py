4bf6b7456593f54a4ab1ab6213a9193de16720dc
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2026-01-09 21:16:22 +0100

    feat: add src/ init file

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""PyTrebuchet: a Python package for simulating trebuchet mechanics."""
+from environment import EnvironmentConfig
+from physical_constants import PhysicalConstants
+
+__all__ = [
+    "EnvironmentConfig",
+    "PhysicalConstants"
+    ]
