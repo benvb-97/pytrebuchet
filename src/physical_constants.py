c472acfeaf79b2b0f409b726df46ec4caae55c23
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2026-01-09 21:14:42 +0100

    feat: add physical constants

---
+++ src/physical_constants.py
@@ -0,0 +1,16 @@
+"""Module defining physical constants for simulations."""
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class PhysicalConstants:
+    """Class for holding physical constants used in simulations."""
+
+    # Standard air density at sea level in kg/m^3
+    AIR_DENSITY: float = 1.225  # kg/m^3
+
+    # Approximate air kinematic viscosity at 15 degrees Celsius at sea level in m^2/s
+    AIR_KINEMATIC_VISCOSITY: float = 1.47e-5  # m^2/s
+
+    # Gravitational acceleration on Earth in m/s^2
+    GRAVITATIONAL_ACCELERATION_EARTH: float = 9.81  # m/s^2
