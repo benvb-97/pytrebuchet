6a6a08467a50c752a74d3588f76035166d1fe0c4
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2026-01-09 23:24:36 +0100

    feat: add imports to project __init__ file

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,15 @@
 from environment import EnvironmentConfig
 from physical_constants import PhysicalConstants
 from projectile import Projectile
+from simulation import Simulation, SimulationPhases
+from trebuchet import HingedCounterweightTrebuchet, WhipperTrebuchet
 
 __all__ = [
     "EnvironmentConfig",
+    "HingedCounterweightTrebuchet",
     "PhysicalConstants",
     "Projectile",
+    "Simulation",
+    "SimulationPhases",
+    "WhipperTrebuchet",
 ]
