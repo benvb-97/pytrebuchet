ed67715bcbe02728e9d8b717376a1b80e75426f7
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2026-01-10 15:31:15 +0100

    feat: add imports to plotting module's __init__ file

--- src/plotting/__init__.py
+++ src/plotting/__init__.py
@@ -1,1 +1,9 @@
 """Plotting utilities for trebuchet simulations."""
+
+from .initial_position import plot_initial_position
+from .launch import animate_launch
+
+__all__ = [
+    "animate_launch",
+    "plot_initial_position",
+]
