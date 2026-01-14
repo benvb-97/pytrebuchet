a6643590a8635d39bfa394f2b0a77b7595411195
LUNA\u0122906 <ben.vanbavel@kuleuven.be>
2026-01-14 21:41:22 +0100

    docs: configure conf.py with extensions, custom html_theme and set root_dir in path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,12 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+from pathlib import Path
+
+root_dir = Path(__file__).parents[2].resolve()
+sys.path.insert(0, str(root_dir))  # Adjust the path to include the project root
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,14 +20,18 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions: list = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # For Google and Numpy style docstrings
+]
 
 templates_path = ["_templates"]
-exclude_patterns: list = []
+exclude_patterns: list[str] = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
